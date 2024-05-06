ff8cdee5a3d11ab4fe42908674d67834dfe1c016
Eduardo Malagutti <145506483+eduMalagutti@users.noreply.github.com>
2024-05-06 15:04:27 -0300

    Fix Taishell.c criado modulo exec_pipe e arrumando a função exec_cd

--- Taishell.c
+++ Taishell.c
@@ -24,12 +24,13 @@
 
 // Declaração das funções
 int exec_command(int argc, char **argv, int redirectionIndex, int pipeIndex);
-int exec_cd(char *argv, char *lastdir);
 void handler(int signal);
+int exec_pipe(int pipeIndex, int argc, char **argv);
+int exec_cd(char *argv, char * curdir ,char *lastdir);
 
 int main()
 {
-    // Variável para guardar o valor do ultimo diretório acessado inicializando com null
+    // Variável para guardar o valor do ultimo diretório acessado inicializando com \0
     char lastdir[PATH_MAX];
     memset(lastdir, '\0', sizeof(lastdir));
 
@@ -39,6 +40,7 @@ int main()
         char cmdline[MAX_COMMAND_LINE];
 
         char **argv = (char **)malloc(MAX_ARGV_SIZE * sizeof(char *));
+
         for (int i = 0; i < MAX_ARGV_SIZE; i++)
         {
             argv[i] = (char *)malloc(MAX_ARG_SIZE * sizeof(char));
@@ -57,14 +59,11 @@ int main()
         int pipeIndex = -1;
 
         // Variável de diretório cwd: Current Working Directory
-        char cwd[PATH_MAX];
-        memset(cwd, '\0', sizeof(cwd));
-
-        // Atualizando o ultimo diretorio acessado
-        strcpy(lastdir, cwd);
+        char curdir[PATH_MAX];
+        memset(curdir, '\0', sizeof(curdir));
 
         // Salvando o diretório atual
-        if (getcwd(cwd, sizeof(cwd)) == NULL) // salva em cwb o path atual
+        if (getcwd(curdir, sizeof(curdir)) == NULL) // salva em curdir o diretório atual
         {
             perror("getcwd() error");
             return 1;
@@ -73,7 +72,7 @@ int main()
         // Printando o prompt
         // \033[1;34mtexto colorido aqui\033[0m
         // Lendo linha de comando
-        printf("\033[1;35mTaishell:\033[0m\033[1;34m~%s\033[0m$ ", cwd);
+        printf("\033[1;35mTaishell:\033[0m\033[1;34m~%s\033[0m$ ", curdir);
         fflush(stdout);
         fgets(cmdline, 50, stdin);
 
@@ -116,7 +115,7 @@ int main()
         // Verifiando comando cd
         if (strcmp(argv[0], "cd") == 0)
         {
-            exec_cd(argv[1], lastdir);
+            exec_cd(argv[1], curdir ,lastdir);
             free(argv);
             continue;
         }
@@ -127,13 +126,6 @@ int main()
         free(argv);
     }
     return 0;
-}
-
-void handler(int signal)
-{
-    int status;
-    while (waitpid(0, &status, WNOHANG) > 0)
-        ;
 }
 
 int exec_command(int argc, char **argv, int redirectionIndex, int pipeIndex)
@@ -160,79 +152,11 @@ int exec_command(int argc, char **argv, int redirectionIndex, int pipeIndex)
 
     if (result == 0)
     { // Filho
+
         // Se o caractere "|" foi encontrado, será feito um pipe
         if (pipeIndex > 0)
         {
-            // Array de descritores de arquivo
-            int pipe_fds[2];
-
-            // Criando os dois descritores de arquivo com a função pipe
-            // pipe_fds[0] será lido
-            // pipe_fds[1] será escrito
-            if (pipe(pipe_fds) == -1)
-            {
-                perror("pipe");
-                return 1; // Retorna código de erro
-            }
-
-            // Criação do segundo processo
-            pid_t result2 = fork();
-            if (result2 == -1)
-            {
-                perror("Erro na criação de processo");
-                return 1; // Retorna código de erro
-            }
-
-            if (result2 == 0)
-            { // Processo filho 2
-                // Fechando o descritor de arquivo que não será usado
-                close(pipe_fds[1]);
-
-                // Mudando o descritor de arquivo padrão
-                dup2(pipe_fds[0], STDIN_FILENO);
-
-                // Criando argv para o segundo processo
-                char *argv2[MAX_ARGV_SIZE];
-                for (int i = 0; i < MAX_ARGV_SIZE; i++)
-                {
-                    argv2[i] = (char *)malloc(MAX_ARG_SIZE * sizeof(char));
-                    if (argv2[i] == NULL)
-                    {
-                        perror("Erro de alocação de memória\n");
-                        exit(1);
-                    }
-                }
-
-                // Copiando os valores para argv2
-                for (int i = pipeIndex + 1, j = 0; i < argc; i++, j++)
-                {
-                    strcpy(argv2[j], argv[i]);
-                }
-                argv2[argc - pipeIndex - 1] = NULL;
-
-                int erroExec = execvp(argv2[0], argv2);
-                if (erroExec == -1)
-                {
-                    perror("execvp");
-                    exit(1);
-                }
-            }
-            else
-            { // Processo filho 1
-                // Fechando o descritor de arquivo que não será usado
-                close(pipe_fds[0]);
-
-                // Mudando o descritor de arquivo padrão
-                dup2(pipe_fds[1], STDOUT_FILENO);
-
-                argv[pipeIndex] = NULL;
-                int erroExec = execvp(argv[0], argv);
-                if (erroExec == -1)
-                {
-                    perror("execvp");
-                    exit(1);
-                }
-            }
+            exec_pipe(pipeIndex, argc, argv);
         }
 
         // Se o caractere ">" for encontrado, será feito um redirecionamento
@@ -253,7 +177,7 @@ int exec_command(int argc, char **argv, int redirectionIndex, int pipeIndex)
         }
 
         // Tirando o caracter de background
-        if(isbackground)
+        if (isbackground)
         {
             argv[argc - 1] = '\0';
         }
@@ -280,9 +204,91 @@ int exec_command(int argc, char **argv, int redirectionIndex, int pipeIndex)
     return 0; // Retorna sucesso
 }
 
-int exec_cd(char *argv, char *lastdir)
+void handler(int signal)
 {
-    char curdir[PATH_MAX];
+    int status;
+    while (waitpid(0, &status, WNOHANG) > 0)
+        ;
+}
+
+int exec_pipe(int pipeIndex, int argc, char **argv)
+{
+    // Array de descritores de arquivo
+    int pipe_fds[2];
+
+    // Criando os dois descritores de arquivo com a função pipe
+    // pipe_fds[0] será lido
+    // pipe_fds[1] será escrito
+    if (pipe(pipe_fds) == -1)
+    {
+        perror("pipe");
+        return 1; // Retorna código de erro
+    }
+
+    // Criação do segundo processo
+    pid_t result2 = fork();
+    if (result2 == -1)
+    {
+        perror("Erro na criação de processo");
+        return 1; // Retorna código de erro
+    }
+
+    if (result2 == 0)
+    { // Processo filho 2
+        // Fechando o descritor de arquivo que não será usado
+        close(pipe_fds[1]);
+
+        // Mudando o descritor de arquivo padrão
+        dup2(pipe_fds[0], STDIN_FILENO);
+
+        // Criando argv para o segundo processo
+        char *argv2[MAX_ARGV_SIZE];
+        for (int i = 0; i < MAX_ARGV_SIZE; i++)
+        {
+            argv2[i] = (char *)malloc(MAX_ARG_SIZE * sizeof(char));
+            if (argv2[i] == NULL)
+            {
+                perror("Erro de alocação de memória\n");
+                exit(1);
+            }
+        }
+
+        // Copiando os valores para argv2
+        for (int i = pipeIndex + 1, j = 0; i < argc; i++, j++)
+        {
+            strcpy(argv2[j], argv[i]);
+        }
+        argv2[argc - pipeIndex - 1] = NULL;
+
+        int erroExec = execvp(argv2[0], argv2);
+        if (erroExec == -1)
+        {
+            perror("execvp");
+            exit(1);
+        }
+    }
+    else
+    { // Processo filho 1
+        // Fechando o descritor de arquivo que não será usado
+        close(pipe_fds[0]);
+
+        // Mudando o descritor de arquivo padrão
+        dup2(pipe_fds[1], STDOUT_FILENO);
+
+        argv[pipeIndex] = NULL;
+        int erroExec = execvp(argv[0], argv);
+        if (erroExec == -1)
+        {
+            perror("execvp");
+            exit(1);
+        }
+    }
+
+    return 0;
+}
+
+int exec_cd(char *argv, char * curdir, char *lastdir)
+{
     char path[PATH_MAX];
 
     // Verifica se é possível obter o diretório atual
@@ -296,7 +302,7 @@ int exec_cd(char *argv, char *lastdir)
     if (argv == NULL)
     {
         // Se o argumento for nulo, muda para o diretório HOME do usuário
-        argv = getenv("HOME");
+        strcpy(argv,getenv("HOME"));
     }
     else if (strcmp(argv, "-") == 0)
     {
@@ -307,7 +313,7 @@ int exec_cd(char *argv, char *lastdir)
             fprintf(stderr, "Nenhum diretório anterior disponível\n");
             return 1;
         }
-        argv = lastdir; // Define o diretório anterior como o destino
+        strcpy(argv, lastdir); // Define o diretório anterior como o destino
     }
     else if (*argv == '~')
     {
@@ -317,7 +323,7 @@ int exec_cd(char *argv, char *lastdir)
             // Se o próximo caractere for "/" ou se o restante da string for vazio,
             // expande para o diretório HOME do usuário seguido do restante do caminho
             snprintf(path, sizeof path, "%s%s", getenv("HOME"), argv + 1);
-            argv = path; // Define o caminho expandido como o destino
+            strcpy(argv,path); // Define o caminho expandido como o destino
         }
         else
         {
